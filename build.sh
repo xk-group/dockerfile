2ea40593390cd8581ca00d41516ca82c12366918
crazyboycjr <crazyboycjr@gmail.com>
2017-06-14 21:12:30 +0800

    change build method

--- build.sh
+++ build.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-#cp /home/cjr/Developing/course-selection/build/libs/course-selection-all.jar .
-cp course-selection-all.jar student-actor
-cp course-selection-all.jar course-actor
-cp course-selection-all.jar http-actor
+cp /home/cjr/Developing/course-selection/build/libs/course-selection-all.jar .
 
-docker rmi student-actor
-docker rmi course-actor
-docker rmi http-actor
-
-docker build -t student-actor student-actor
-docker build -t course-actor course-actor
-docker build -t http-actor http-actor
+docker rmi course-selection
+docker build -t course-selection .
--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ docker rm course-actor-container
 docker rm student-actor-container
 docker rm http-actor-container
 
-docker run -it --rm -p 2552:2552 -d --name student-actor-container student-actor
-docker run -it --rm -p 2553:2553 -d --name course-actor-container course-actor
-docker run -it --rm -p 8000:8000 -d --name http-actor-container http-actor
+
+docker run -it --rm -p 2552:2552 -d --name student-actor-container -v "$PWD"/config-student:/usr/lib/course-selection/config course-selection moe.taiho.course_selection.cluster.ClusterMain
+docker run -it --rm -p 2553:2553 -d --name course-actor-container -v "$PWD"/config-course:/usr/lib/course-selection/config course-selection moe.taiho.course_selection.cluster.ClusterMain
+docker run -it --rm -p 8000:8000 -d --name http-actor-container -v "$PWD"/config-http:/usr/lib/course-selection/config course-selection moe.taiho.course_selection.cluster.WebServer
