6893a1f4b40b700e73271cab0f427103442cbab6
crazyboycjr <crazyboycjr@gmail.com>
2017-06-14 22:00:43 +0800

    update example.conf, update sub.sh

--- sub.sh
+++ sub.sh
@@ -2,6 +2,6 @@
 
 hostname=$1
 
-sed -i "s/tcp.hostname = \"\([^\"]\+\)\"/tcp.hostname = \"$hostname\"/" config-course/example.conf
-sed -i "s/tcp.hostname = \"\([^\"]\+\)\"/tcp.hostname = \"$hostname\"/" config-student/example.conf
-sed -i "s/tcp.hostname = \"\([^\"]\+\)\"/tcp.hostname = \"$hostname\"/" config-http/example.conf
+sed -i "s/tcp.hostname = \"\([^\"]*\)\"/tcp.hostname = \"$hostname\"/" config-course/example.conf
+sed -i "s/tcp.hostname = \"\([^\"]*\)\"/tcp.hostname = \"$hostname\"/" config-student/example.conf
+sed -i "s/tcp.hostname = \"\([^\"]*\)\"/tcp.hostname = \"$hostname\"/" config-http/example.conf
