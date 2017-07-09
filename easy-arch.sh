3c5c59ec02d01b1eee5860a1d097b7cded523a69
Jianfeng Zhang <swordfeng123@gmail.com>
2017-07-09 19:57:08 +0800

    update deploy file

--- easy-arch.sh
+++ easy-arch.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-sudo pacman -S git docker --noconfirm --needed
-sudo gpasswd -a $USER docker
-sudo systemctl enable docker
-sudo systemctl start docker
+main() {
+    sudo pacman -S git docker --noconfirm --needed
+    sudo gpasswd -a $USER docker
+    sudo systemctl enable docker
+    sudo systemctl start docker
 
-rm -rf dockerfile
-git clone https://github.com/xk-group/dockerfile.git
-cd dockerfile
-git checkout master
+    rm -rf dockerfile
+    git clone https://github.com/xk-group/dockerfile.git
+    cd dockerfile
+    git checkout master
+
+    export -f run_as_docker
+    sg docker run_as_docker
+}
 
 run_as_docker() {
-    #docker rmi crazyboycjr/course-selection
+    docker rmi crazyboycjr/course-selection
     #docker pull crazyboycjr/course-selection
     #docker tag crazyboycjr/course-selection course-selection
-
-    ./sub.sh 172.104.99.30 172.104.127.88
-    ./run.sh
+    curl https://dl.dropboxusercontent.com/s/wv3xvq8076wsqe3/course-selection.tar.xz | xz -d -c - | docker load
 }
-export -f run_as_docker
-sg docker run_as_docker
 
+main
